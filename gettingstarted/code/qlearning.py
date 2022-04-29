143ebcf4558fa23d9e69c0dff1f54330124229f3
Theaveas So <theaveas.so@gmail.com>
2022-04-30 00:16:16 +0800

    q learning algo

--- gettingstarted/code/qlearning.py
+++ gettingstarted/code/qlearning.py
@@ -3,12 +3,28 @@ import numpy as np
 import gym
 
 # create an environment
-env = gym.make('mountain_car_v0')
+env = gym.make('MountainCar-v0')
 env.reset()
 
-for _ in range(1):
-    done = False
-    env.reset()
+# state argreation 
+# turn a continuos task into episodic task
+GRID_SIZE = [20] * 2
+win_size = (env.observation_space.high - env.observation_space.low) / GRID_SIZE
+
+print(GRID_SIZE)
+
+def test_env():
+    for _ in range(1):
+        done = False
+        env.reset()
+        
+        while not done:
+            _, _, done, _ = env.step(env.action_space.sample())
+            env.render()
+            
+    env.close()
     
-    while not done:
-        _, _, done, _ = env.step(env.action_space.sameple())
+    
+
+if __name__ == "__main__":
+    pass
